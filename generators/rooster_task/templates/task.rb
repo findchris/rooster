65d4d8927c258bdfd2bfecf05d91b61244a3410e
Chris Johnson <findchris@gmail.com>
2009-06-08 19:15:33 -0700

    Commit the updated task template with better logging.

--- generators/rooster_task/templates/task.rb
+++ generators/rooster_task/templates/task.rb
@@ -1,11 +1,13 @@
 class <%= class_name %>Task < Rooster::Task
   define_schedule do |s|
-    s.every "10s", :tags => self.name do
-  
-      # Your code here, eg: User.send_due_invoices!
-      log "I'm running every 10 seconds! #{Time.now.to_s(:db)}" # delete me, really. :D
-
-      # this is required to keep the tasks from eating all the free connections:
+    s.every "10s", :tags => self.name do      
+      log "#{self.name} starting at #{Time.now.to_s(:db)}"
+      
+      ###
+      # Your code here (e.g. User.send_due_invoices!)
+      ###
+      
+      log "#{self.name} completed at #{Time.now.to_s(:db)}"
       ActiveRecord::Base.connection_pool.release_connection
     end
   end
