c397d344f3489127350098ba3e29f693bdb952dd
Chris Johnson <findchris@gmail.com>
2009-06-08 18:46:58 -0700

    Always copy the daemon file to ensure the latest version (add :force => true to copy_file).

--- install.rb
+++ install.rb
@@ -12,7 +12,7 @@ begin
   
   FileUtils.mkdir_p(destination_dir)
     
-  FileUtils.copy_file(source_file, destination_file)
+  FileUtils.copy_file(source_file, destination_file, :force => true)
   
   puts File.read(File.join(File.dirname(__FILE__), 'README.markdown'))
   
