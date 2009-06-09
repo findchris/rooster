caf2b56ded693192d8e1ce1eea21128db7829870
Chris Johnson <findchris@gmail.com>
2009-06-08 19:06:08 -0700

    Commit the updated task template.

--- generators/rooster_task/templates/task.rb
+++ generators/rooster_task/templates/task.rb
@@ -1,17 +1,12 @@
 class <%= class_name %>Task < Rooster::Task
-  class << self
-    
-    def schedule      
-      # see rufus-scheduler documentation for more information on what methods scheduler can handle
-      scheduler.every "10s", :tags => self.name do
-        
-        # Your code here, eg: User.send_due_invoices!
-        log "I'm running every 10 seconds! #{Time.now.to_s(:db)}" # delete me, really. :D
-        
-        # this is required to keep the tasks from eating all the free connections:
-        ActiveRecord::Base.connection_pool.release_connection
-      end
+  define_schedule do |s|
+    s.every "10s", :tags => self.name do
+  
+      # Your code here, eg: User.send_due_invoices!
+      log "I'm running every 10 seconds! #{Time.now.to_s(:db)}" # delete me, really. :D
+
+      # this is required to keep the tasks from eating all the free connections:
+      ActiveRecord::Base.connection_pool.release_connection
     end
-    
   end
 end
