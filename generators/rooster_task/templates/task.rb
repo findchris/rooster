d4adc375bcddc7335e2a843ad09c4d186f64cbdc
Chris Johnson <findchris@gmail.com>
2009-07-22 16:24:13 -0700

    Added ActiveRecord::Base.connection.reconnect! to task generator to fix mysql has gone away errors.

--- generators/rooster_task/templates/task.rb
+++ generators/rooster_task/templates/task.rb
@@ -3,6 +3,7 @@ class <%= class_name %>Task < Rooster::Task
     s.every "1d", :first_at => Chronic.parse("next 2:00am"), :tags => [self.name] do  # refer to:  http://github.com/jmettraux/rufus-scheduler/tree/master
       begin
         log "#{self.name} starting at #{Time.now.to_s(:db)}"
+        ActiveRecord::Base.connection.reconnect!
 
         ###
         # Your code here (e.g. User.send_due_invoices!)
