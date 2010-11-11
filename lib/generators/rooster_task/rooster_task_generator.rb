69e83e735c51a0c0b6e0cc0401ff9ef414048fe1
Frederick Ros <frederick.ros@gmail.com>
2010-11-11 23:18:30 +0100

    Preliminary support of generator for Rails 3

---
+++ lib/generators/rooster_task/rooster_task_generator.rb
@@ -0,0 +1,3 @@
+class RoosterTaskGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+end
---
+++ lib/generators/rooster_task/templates/task.rb
@@ -0,0 +1,21 @@
+class <%= class_name %>Task < Rooster::Task
+  
+  @tags = ['<%= class_name %>'] # CUSTOMIZE:  add additional tags here
+  
+  define_schedule do |s|
+    s.every "1d", :first_at => Chronic.parse("next 2:00am"), :tags => @tags do  # CUSTOMIZE:  reference http://github.com/jmettraux/rufus-scheduler/tree/master
+      begin
+        log "#{self.name} starting at #{Time.now.to_s(:db)}"
+        ActiveRecord::Base.connection.reconnect!
+
+        ###
+        # CUSTOMIZE:  Your code here (e.g. User.send_due_invoices!)
+        ###
+        
+      ensure
+        log "#{self.name} completed at #{Time.now.to_s(:db)}"
+        ActiveRecord::Base.connection_pool.release_connection
+      end
+    end
+  end
+end
