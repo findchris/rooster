c74b33ac20e88e2f1d4159aeddd2738fcc7359e3
Frédérick Ros <frederick.ros@gmail.com>
2010-11-12 01:04:00 +0100

    Ensure task file is correctly generated

--- lib/generators/rooster_task/rooster_task_generator.rb
+++ lib/generators/rooster_task/rooster_task_generator.rb
@@ -1,3 +1,8 @@
 class RoosterTaskGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+  argument :task_name, :type => :string
+  
+  def generate_task
+    template "task.rb", "lib/rooster/tasks/#{task_name.underscore}_task.rb"
+  end
 end
